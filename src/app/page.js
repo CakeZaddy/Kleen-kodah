11573d4cf2367d560c39899086edd8c694d98faa
Sulaimon Omotosho <susudef2u@gmail.com>
2023-06-07 15:10:30 +0100

    solved legacy peer deps

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,12 @@
 import { PostCard, PostWidgets, Categories } from '../components'
 import { getPosts } from '../Services'
 
-export async function getStaticProps() {
-  const posts = (await getPosts()) || []
-
-  return {
-    props: { posts },
-  }
-}
-
 export default function Home({ posts }) {
   return (
     <main className='container mx-auto px-10 mb-8'>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='col-span-1 lg:col-span-8'>
-          {posts.map((post) => (
+          {posts?.map((post) => (
             <PostCard post={post.node} key={post.title} />
           ))}
         </div>
@@ -27,4 +19,12 @@ export default function Home({ posts }) {
       </div>
     </main>
   )
+}
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || []
+
+  return {
+    props: { posts },
+  }
 }
